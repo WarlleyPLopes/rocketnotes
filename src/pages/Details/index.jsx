dd3a50c4a6b969236ecf695ae0fb58c65fd44194
Warlley Lopes <warlleylopes123@gmail.com>
2024-03-20 16:31:03 +0000

    created component tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from './styles'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
 
 export function Details() {
   return (
@@ -18,6 +19,13 @@ export function Details() {
             <a href="#">https://rocketseat.com.br</a>
           </li>
         </Links>
+
+        <Section title="Marcadores">
+        <Tag title="express"/>
+        <Tag title="nodejs"/>
+          
+
+        </Section>
       </Section>
 
       <Button title="Voltar" />
