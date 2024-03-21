04bcc84ab30c81d62348cde6945e147216d2e2a6
Warlley Lopes <warlleylopes123@gmail.com>
2024-03-21 12:31:47 +0000

    created component input

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,9 @@
-import {FiPlus} from 'react-icons/fi'
+import { FiPlus } from "react-icons/fi";
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
 import { Header } from "../../components/Header";
 import { ButtonText } from "../../components/ButtonText";
+import { Input } from "../../components/Input";
 
 export function Home() {
   return (
@@ -15,10 +16,7 @@ export function Home() {
 
       <Menu>
         <li>
-          <ButtonText 
-            title="Todos" 
-            isActive
-          />
+          <ButtonText title="Todos" isActive />
         </li>
         <li>
           <ButtonText title="React" />
@@ -28,12 +26,14 @@ export function Home() {
         </li>
       </Menu>
 
-      <Search></Search>
+      <Search>
+        <Input placeholder="Pesquisar pelo titulo" />
+      </Search>
 
       <Content></Content>
 
       <NewNote>
-        <FiPlus/>
+        <FiPlus />
         Criar nota
       </NewNote>
     </Container>
